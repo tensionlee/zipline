50e4eb5e66b1331f10da90b48a311dfb300716f7
Maya Tydykov <mtydykov@quantopian.com>
2016-03-10 16:18:37 -0500

    WIP: update tests.

--- tests/pipeline/test_dividends.py
+++ tests/pipeline/test_dividends.py
@@ -106,28 +106,29 @@ def get_values_for_date_ranges(zip_vals_dates,
     )
 
 
-def get_previous_amounts_for_dates(zip_with_floats_dates,
+def get_previous_vals_for_dates(zip_with_floats_dates,
                                    num_days_between_dates,
                                    dates,
-                                   date_invervals):
+                                   date_invervals,
+                                   vals):
     return pd.DataFrame({
             0: get_values_for_date_ranges(zip_with_floats_dates,
                                           num_days_between_dates,
-                                          ['NaN', 1, 15],
+                                          vals[0],
                                           date_invervals[0]),
             1: get_values_for_date_ranges(zip_with_floats_dates,
                                           num_days_between_dates,
-                                          ['NaN', 7, 13],
+                                          vals[1],
                                           date_invervals[1]),
             2: get_values_for_date_ranges(zip_with_floats_dates,
                                           num_days_between_dates,
-                                          ['NaN', 3, 1],
+                                          vals[2],
                                           date_invervals[2]),
             # Assume the latest of 2 cash values is used if we find out about 2
             # announcements that happened on the same day for the same sid.
             3: get_values_for_date_ranges(zip_with_floats_dates,
                                           num_days_between_dates,
-                                          ['NaN', 23],
+                                          vals[3],
                                           date_invervals[3]),
             4: zip_with_floats_dates(['NaN'] * len(dates)),
         }, index=dates)
@@ -195,30 +196,20 @@ class DividendsByAnnouncementDateTestCase(TestCase, EventLoaderCommonMixin):
                [None, '2014-01-04'], ['2014-01-05', None]
             ]
         ]
+        announcement_dates = [['NaT', '2014-01-04', '2014-01-09'],
+                 ['NaT', '2014-01-04', '2014-01-09'],
+                 ['NaT', '2014-01-04', '2014-01-14'],
+                 ['NaT', '2014-01-04']]
+        amounts = [['NaN', 1, 15], ['NaN', 7, 13], ['NaN', 3, 1], ['NaN', 23]]
 
-        self.cols[PREVIOUS_ANNOUNCEMENT] = pd.DataFrame({
-            0: get_values_for_date_ranges(zip_with_dates_for_dates,
-                                          num_days_between_for_dates,
-                                          ['NaT', '2014-01-04', '2014-01-09'],
-                                          date_intervals[0]),
-            1: get_values_for_date_ranges(zip_with_dates_for_dates,
-                                          num_days_between_for_dates,
-                                          ['NaT', '2014-01-04', '2014-01-09'],
-                                          date_intervals[1]),
-            2: get_values_for_date_ranges(zip_with_dates_for_dates,
-                                          num_days_between_for_dates,
-                                          ['NaT', '2014-01-04', '2014-01-14'],
-                                          date_intervals[2]),
-            3: get_values_for_date_ranges(zip_with_dates_for_dates,
-                                          num_days_between_for_dates,
-                                          ['NaT', '2014-01-04'],
-                                          date_intervals[3]),
-            4: zip_with_dates_for_dates(['NaT'] * len(dates)),
-        }, index=dates)
+        self.cols[PREVIOUS_ANNOUNCEMENT] = get_previous_vals_for_dates(
+            zip_with_dates_for_dates, num_days_between_for_dates, dates,
+            date_intervals, announcement_dates
+        )
 
-        self.cols[PREVIOUS_AMOUNT] = get_previous_amounts_for_dates(
+        self.cols[PREVIOUS_AMOUNT] = get_previous_vals_for_dates(
             zip_with_floats_dates, num_days_between_dates, dates,
-            date_intervals
+            date_intervals, amounts
         )
 
         self.cols[
@@ -232,7 +223,7 @@ class BlazeDividendsByAnnouncementDateTestCase(
     @classmethod
     def setUpClass(cls):
         super(BlazeDividendsByAnnouncementDateTestCase, cls).setUpClass()
-        cls.loader_type = DividendsByAnnouncementDateLoader
+        cls.loader_type = BlazeDividendsByAnnouncementDateLoader
 
     def loader_args(self, dates):
         _, mapping = super(
@@ -312,40 +303,88 @@ class DividendsByExDateTestCase(TestCase, EventLoaderCommonMixin):
     def setup(self, dates):
         zip_with_floats_dates = partial(self.zip_with_floats, dates)
         num_days_between_dates = partial(self.num_days_between, dates)
-        self.cols[NEXT_EX_DATE] = self.get_expected_previous_event_dates(dates)
-        self.cols[PREVIOUS_EX_DATE] = self.get_expected_previous_event_dates(
-            dates
+        num_days_between_for_dates = partial(self.num_days_between, dates)
+        zip_with_dates_for_dates = partial(self.zip_with_dates, dates)
+
+        prev_date_intervals = [
+            [
+                [None, '2014-01-14'], ['2014-01-15', '2014-01-19'],
+                ['2014-01-20', None]
+            ],
+            [
+                [None, '2014-01-14'], ['2014-01-15', '2014-01-19'],
+                ['2014-01-20', None]
+            ],
+            [
+               [None, '2014-01-09'], ['2014-01-10', '2014-01-19'],
+               ['2014-01-10', None]
+            ],
+            [
+               [None, '2014-01-09'], ['2014-01-10', '2014-01-14'],
+               ['2014-01-15', None]
+            ]
+        ]
+
+        next_date_intervals = [
+            [
+                [None, '2014-01-04'], ['2014-01-05', '2014-01-15'],
+                ['2014-01-15', '2014-01-20'], ['2014-01-21', None]
+            ],
+            [
+                [None, '2014-01-04'], ['2014-01-05', '2014-01-09'],
+                ['2014-01-10', '2014-01-15'], ['2014-01-16', '2014-01-20'],
+                ['2014-01-21', None]
+            ],
+            [
+                [None, '2014-01-04'], ['2014-01-05', '2014-01-10'],
+                ['2014-01-11', '2014-01-14'], ['2014-01-15', '2014-01-20'],
+                ['2014-01-21', None]
+            ],
+            [
+                [None, '2014-01-04'], ['2014-01-05', '2014-01-10'],
+                ['2014-01-11', '2014-01-15'], ['2014-01-16', None]
+            ]
+        ]
+
+        next_announcement_dates = [['NaT', '2014-01-15', '2014-01-20',
+                                           'NaT'],
+                                   ['NaT', '2014-01-20', '2014-01-15',
+                                           '2014-01-20', 'NaT'],
+                                   ['NaT', '2014-01-10', '2014-01-14',
+                                           '2014-01-20', 'NaT'],
+                                   ['NaT', '2014-01-10', '2014-01-15',
+                                           'NaT']]
+        self.cols[NEXT_EX_DATE] = get_previous_vals_for_dates(
+            zip_with_dates_for_dates, num_days_between_for_dates, dates,
+            next_date_intervals, next_announcement_dates
+        )
+        prev_announcement_dates = [['NaT', '2014-01-15', '2014-01-20'],
+                                   ['NaT', '2014-01-15', '2014-01-20'],
+                                   ['NaT', '2014-01-10', '2014-01-20'],
+                                   ['NaT', '2014-01-10', '2014-01-15']]
+        self.cols[PREVIOUS_EX_DATE] = get_previous_vals_for_dates(
+            zip_with_dates_for_dates, num_days_between_for_dates, dates,
+            prev_date_intervals, prev_announcement_dates
         )
 
-        # TODO: fix amounts for next/previous to correct ones
-        _expected_next_amount = pd.DataFrame({
-            0: zip_with_floats_dates(
-                ['NaN'] * num_days_between_dates(None, '2014-01-14') +
-                [1] * num_days_between_dates('2014-01-15', '2014-01-19') +
-                [15] * num_days_between_dates('2014-01-20', None)
-            ),
-            1: zip_with_floats_dates(
-                ['NaN'] * num_days_between_dates(None, '2014-01-14') +
-                [13] * num_days_between_dates('2014-01-15', '2014-01-19') +
-                [7] * num_days_between_dates('2014-01-20', None)
-            ),
-            2: zip_with_floats_dates(
-                ['NaN'] * num_days_between_dates(None, '2014-01-09') +
-                [3] * num_days_between_dates('2014-01-10', '2014-01-19') +
-                [1] * num_days_between_dates('2014-01-20', None)
-            ),
-            3: zip_with_floats_dates(
-                ['NaN'] * num_days_between_dates(None, '2014-01-09') +
-                [6] * num_days_between_dates('2014-01-10', '2014-01-14') +
-                [23] * num_days_between_dates('2014-01-15', None)
-            ),
-            4: zip_with_floats_dates(['NaN'] * len(dates)),
-        }, index=dates)
-        self.cols[NEXT_AMOUNT] = _expected_next_amount
-
-        self.cols[PREVIOUS_AMOUNT] = get_previous_amounts_for_dates(
-            zip_with_floats_dates, num_days_between_dates, dates
+        next_amounts = [['NaN', 1, 15, 'NaN'],
+                        ['NaN', 7, 13, 7, 'NaN'],
+                        ['NaN', 3, 'NaN', 1, 'NaN'],
+                        ['NaN', 6, 23, 'NaN']]
+        self.cols[NEXT_AMOUNT] = get_previous_vals_for_dates(
+            zip_with_floats_dates, num_days_between_dates,
+            dates, next_date_intervals, next_amounts
         )
+
+        prev_amounts = [['NaN', 1, 15],
+                        ['NaN', 13, 7],
+                        ['NaN', 3, 1],
+                        ['NaN', 6, 23]]
+        self.cols[PREVIOUS_AMOUNT] = get_previous_vals_for_dates(
+            zip_with_floats_dates, num_days_between_dates,
+            dates, prev_date_intervals, prev_amounts
+        )
+
         self.cols[DAYS_SINCE_PREV_DIVIDEND_ANNOUNCEMENT] = \
             self._compute_busday_offsets(
                 self.cols[PREVIOUS_ANNOUNCEMENT]
